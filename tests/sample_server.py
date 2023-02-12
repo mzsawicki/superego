368c67737bed65821f4fb033f6a4a81c54082045
Marcin Sawicki <marcin.z.sawicki@protonmail.com>
2023-02-12 14:44:07 +0100

    Add more players to sample session

--- tests/sample_server.py
+++ tests/sample_server.py
@@ -3,7 +3,7 @@ from superego.infrastructure.websockets.assemble\
     import assemble_websockets_server
 from superego.game.game import Lobby, LobbyMember, GameSettings
 from tests.test_deck import test_deck
-from uuid import UUID
+from uuid import UUID, uuid4
 import logging
 
 HOST = '0.0.0.0'
@@ -19,6 +19,13 @@ if __name__ == '__main__':
     guest_1._guid = UUID('a7da476f-c81b-4bc7-ac17-e7e675ccc95f')
     guest_2 = LobbyMember('Drew')
     guest_2._guid = UUID('d3299cf9-3305-41e9-b2b1-bfdd3e22a7c2')
+    guest_3 = LobbyMember('Mary')
+    guest_3._guid = uuid4()
+    guest_4 = LobbyMember('Mike')
+    guest_4._guid = uuid4()
+    guest_5 = LobbyMember('Gary')
+    guest_5._guid = uuid4()
+    
 
     deck = test_deck
 
@@ -27,6 +34,9 @@ if __name__ == '__main__':
     lobby = Lobby(host, game_settings)
     lobby.add_member(guest_1)
     lobby.add_member(guest_2)
+    lobby.add_member(guest_3)
+    lobby.add_member(guest_4)
+    lobby.add_member(guest_5)
 
     config = WebsocketsServerConfig(host=HOST, port=PORT, encoding=ENCODING)
 
